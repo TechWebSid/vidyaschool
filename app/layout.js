f84b4c23cacdc49674023a0f15d7dd1cabdf3dd7
TechWebSid <siddharthasrivastava30@gmail.com>
2025-03-15 11:16:23 +0530

    wtsp button

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import WhatsAppButton from '@/components/WhatsAppButton'
 
 export const metadata = {
   title: 'Vidya International School',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <WhatsAppButton />
+      </body>
     </html>
   )
 }
