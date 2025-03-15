514cf1104b97730847717cb6614490b11e8d4015
TechWebSid <siddharthasrivastava30@gmail.com>
2025-03-15 11:42:01 +0530

    faviicon

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,16 @@ import WhatsAppButton from '@/components/WhatsAppButton'
 export const metadata = {
   title: 'Vidya International School',
   description: 'Excellence in Education - Kindergarten, CBSE School, and IIT-JEE Coaching',
+  icons: [
+    {
+      rel: 'icon',
+      url: '/favicon.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      url: '/favicon.png',
+    }
+  ],
 }
 
 export default function RootLayout({ children }) {
